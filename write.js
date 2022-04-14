37b772ad0c9a654f786cfd961199fa3ded6f890a
Sol <darkmaster2210@gmail.com>
2022-04-14 19:05:06 +0200

    Added write_append

--- write.js
+++ write.js
@@ -13,11 +13,22 @@ async function write(s){
 		
 }
 
+async function write_append(s){
+	const writeable = document.getElementById("writeable");
+	writeable.innerHTML = writeable.innerHTML.split("<")[0];
+	const write_letter = async (c,wa) => {
+		wa.innerHTML += c;
+	}
+	for (let i = 0; i < s.length; i++){
+		let c = s.charAt(i);
+		await mySleepFunction(100);
+		write_letter(c, writeable);
+	}
+	writeable.innerHTML += " <span class='blinking-cursor'>|</span>"
+		
+}
+
 function mySleepFunction(delayTime) {
 	return new Promise(resolve => setTimeout(resolve, delayTime + Math.floor(Math.random()*100)));
 }
 
-async function get_modify(){
-	const modify = Math.floor(Math.random()*50);
-	console.log(100 + modify)
-}
